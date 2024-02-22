bbcc813938b3090d55f6f11dc75568a07db8bcd1
dp-dvlpr <160069530+dp-dvlpr@users.noreply.github.com>
2024-02-22 13:51:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,18 @@
 import streamlit
 import snowflake.connector
+import pandas
 
+# title for the web app
 streamlit.title("Zena's Amazing Athleisure Catalog")
 
+# connect to Snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
 my_cur = my_cnx.cursor() 
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()") 
-my_data_row = my_cur.fetchone() 
-streamlit.text("Hello from Snowflake:") 
-streamlit.text(my_data_row)
+
+# query snowflake to get colors and styles
+my_cur.execute("select color_or_style from catalog_for_website")
+my_catalog = my_cur.fetchall()
+
+# display the data in a dataframe
+df = pandas.DataFrame(my_catalog)
+streamlit.write(df)
