a04c752e508a3e8f8148ee4ff4e03449577a2a14
dp-dvlpr <160069530+dp-dvlpr@users.noreply.github.com>
2024-02-22 13:58:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,17 @@ my_catalog = my_cur.fetchall()
 
 # get the data into a dataframe
 df = pandas.DataFrame(my_catalog)
-streamlit.write(df)
+# streamlit.write(df)
 
 # display the colors in a list
 color_list = df[0].values.tolist()
 option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
+product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
+
+# query Snowflake for the image url, price, etc
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';") 
+df2 = my_cur.fetchone() 
+# display the product image, etc
+streamlit.image( df2[0], width=400, caption= product_caption ) 
+streamlit.write('Price: ', df2[1]) 
+streamlit.write('Sizes Available: ',df2[2]) streamlit.write(df2[3])
